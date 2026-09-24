f474b283385ef4e13f5682f362edca816a7d0f90
James Martin <oss@jiiim.com>
2026-09-23 23:20:33 -0400

    chore: update ds-menu-bar to 0.0.10

--- Casks/ds-menu-bar.rb
+++ Casks/ds-menu-bar.rb
@@ -1,6 +1,6 @@
 cask "ds-menu-bar" do
-  version "0.0.9"
-  sha256 "ea8adefb33543e976860b594caa17c50e601599cc8e7b2e52e0a3a9275d92ba6"
+  version "0.0.10"
+  sha256 "8f99eb5b945ff2d793cdfd61eadb191b3eb280666135d8b1c6da42c5bb97068d"
 
   url "https://github.com/jiiim/ds-menu-bar/releases/download/v#{version}/DS-Menu-Bar-v#{version}-arm64.dmg"
   name "DS Menu Bar"
